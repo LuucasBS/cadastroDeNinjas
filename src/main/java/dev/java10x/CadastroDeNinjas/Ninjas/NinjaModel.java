b3733b98ab4e12dc8a155fde2b68d0b2b7251838
LucasBorges <lucasborges9002@hotmail.com>
2025-07-10 20:57:14 -0300

    fix: adicionar mudança commit email

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -16,8 +16,12 @@ public class NinjaModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+
     String nome;
+
+    @Column(unique = true)
     String email;
+
     int idade;
 
     @ManyToOne()
